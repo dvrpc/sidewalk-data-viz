f90a8fbd406dbfd559178182a0f5d39399a375b0
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-02-18 15:41:26 -0500

    add all vector sources

--- js/mapSources.js
+++ js/mapSources.js
@@ -1,9 +1,25 @@
-const sources = {  
-    'boundaries': {
-        type: 'vector',
-        url: 'https://tiles.dvrpc.org/data/dvrpc-municipal.json'
-    }
-    // add more sources here
-}
+const sources = {
+  boundaries: {
+    type: "vector",
+    url: "https://tiles.dvrpc.org/data/dvrpc-municipal.json",
+  },
+  sidewalk_inventory: {
+    type: "vector",
+    url: "https://tiles.dvrpc.org/data/pedestrian-network.json",
+  },
+  ped_analysis: {
+    type: "vector",
+    url: "https://tiles.dvrpc.org/data/sidewalk-gaps-analysis.json",
+    // url: "http://0.0.0.0:8080/data/sidewalk_gaps_analysis.json",
+  },
+  ridescore_analysis: {
+    type: "vector",
+    url: "https://tiles.dvrpc.org/data/ridescore-analysis.json",
+  },
+  regional_boundaries: {
+    type: "vector",
+    url: "https://tiles.dvrpc.org/data/dvrpc-municipal.json",
+  },
+};
 
-export default sources
+export default sources;
