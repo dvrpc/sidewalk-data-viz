bf44a19c7351a1974b2de5028444e39f2b241498
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-02-19 11:27:07 -0500

    add geocoder

--- js/map.js
+++ js/map.js
@@ -1,60 +1,73 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibW1vbHRhIiwiYSI6ImNqZDBkMDZhYjJ6YzczNHJ4cno5eTcydnMifQ.RJNJ7s7hBfrJITOBZBdcOA'
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibW1vbHRhIiwiYSI6ImNqZDBkMDZhYjJ6YzczNHJ4cno5eTcydnMifQ.RJNJ7s7hBfrJITOBZBdcOA";
 
 const initMap = () => {
-    const longitudeOffset = window.innerWidth > 800 ? -75.8 : -75.2273
-    const zoom = window.innerWidth <= 420 ? 7.3 : 8.3
+  const longitudeOffset = window.innerWidth > 800 ? -75.8 : -75.2273;
+  const zoom = window.innerWidth <= 420 ? 7.3 : 8.3;
 
-    return new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/dark-v10',
-        center: [longitudeOffset, 40.071],
-        zoom: zoom
-    })
-}
+  return new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/dark-v10",
+    center: [longitudeOffset, 40.071],
+    zoom: zoom,
+  });
+};
 
-const makeRegionalExtentControl = map => {
-    const longitudeOffset = window.innerWidth > 800 ? -75.8 : -75.2273
-    const zoom = window.innerWidth <= 420 ? 7.3 : 8.3
+const makeRegionalExtentControl = (map) => {
+  const longitudeOffset = window.innerWidth > 800 ? -75.8 : -75.2273;
+  const zoom = window.innerWidth <= 420 ? 7.3 : 8.3;
 
-    const dvrpcExtent = {
-        center: [longitudeOffset, 40.0518322],
-        zoom: zoom
-    }
+  const dvrpcExtent = {
+    center: [longitudeOffset, 40.0518322],
+    zoom: zoom,
+  };
 
-    const navigationControl = new mapboxgl.NavigationControl();
+  const navigationControl = new mapboxgl.NavigationControl();
 
-    // create custom button elements
-    const button = document.createElement('button')
-    const icon = document.createElement('img')
+  // create custom button elements
+  const button = document.createElement("button");
+  const icon = document.createElement("img");
 
-    button.type = 'button'
-    icon.id = 'regional-extent-img'
-    icon.alt = 'DVRPC Alternative Logo'
-    icon.src = 'https://www.dvrpc.org/img/banner/new/bug-favicon.png'
+  button.type = "button";
+  icon.id = "regional-extent-img";
+  icon.alt = "DVRPC Alternative Logo";
+  icon.src = "https://www.dvrpc.org/img/banner/new/bug-favicon.png";
 
-    button.classList.add('mapboxgl-ctrl-icon')
-    button.classList.add('mapboxgl-ctrl-dvrpc')
+  button.classList.add("mapboxgl-ctrl-icon");
+  button.classList.add("mapboxgl-ctrl-dvrpc");
 
-    button.setAttribute('aria-label', 'Default DVRPC Extent')
+  button.setAttribute("aria-label", "Default DVRPC Extent");
 
-    button.onclick = () => map.flyTo({center: dvrpcExtent.center, zoom: dvrpcExtent.zoom}) 
+  button.onclick = () =>
+    map.flyTo({ center: dvrpcExtent.center, zoom: dvrpcExtent.zoom });
 
-    button.appendChild(icon)
+  button.appendChild(icon);
 
-    // plug into mapbox fncs
-    navigationControl._extent = button
-    navigationControl._container.appendChild(button)
+  // plug into mapbox fncs
+  navigationControl._extent = button;
+  navigationControl._container.appendChild(button);
 
-    return navigationControl
-}
+  return navigationControl;
+};
 
 const makeMap = () => {
-    const map = initMap()
-    const control = makeRegionalExtentControl(map)
+  const map = initMap();
 
-    map.addControl(control);
+  // Add a geocoder to the map. Limit the bbox to the DVRPC region.
+  var geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+    marker: true,
+    placeholder: "Type here to zoom to...",
+    bbox: [-76.210785, 39.478606, -73.885803, 40.601963],
+  });
 
-    return map
-}
+  map.addControl(geocoder);
 
-export default makeMap
+  const control = makeRegionalExtentControl(map);
+  map.addControl(control);
+
+  return map;
+};
+
+export default makeMap;
