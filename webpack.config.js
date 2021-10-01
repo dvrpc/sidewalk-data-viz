40246ef445af5529dfdae524d9155a391073ee05
aaronfraint <aaronfraint@icloud.com>
2021-10-01 14:43:20 -0400

    build to "/docs" folder

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
+    path: path.resolve(__dirname, "docs"),
   },
   plugins: [
     new CleanWebpackPlugin(),
